7c1a894fce310693e2503f6270df1fc9b005f268
XaM1990 <m31353@yandex.ru>
2025-11-03 11:56:47 +0500

    создание секции

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,5 +1,5 @@
-(function() {
-    document.addEventListener('click', burgerInit )
+(function () {
+    document.addEventListener('click', burgerInit)
 
     function burgerInit(e) {
         const burgerIcon = e.target.closest('.burger-icon')
@@ -7,13 +7,39 @@
         if (!burgerIcon && !burgerNavLink) return
         if (document.documentElement.clientWidth > 900) return
 
-        if(!document.body.classList.contains('body--opened-menu')){
+        if (!document.body.classList.contains('body--opened-menu')) {
             document.body.classList.add('body--opened-menu')
         } else {
             document.body.classList.remove('body--opened-menu')
         }
     }
 
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                document.body.classList.remove('body--opened-modal')
+            }
+        })
 
 
+    }
 })()
