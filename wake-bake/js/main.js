dd96a3b897590508367b6f94dbeba79d2f8fd564
XaM1990 <m31353@yandex.ru>
2025-12-19 13:36:40 +0500

    layout testimonials

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -143,4 +143,33 @@
         }
     });
 
+    // Слайдер-галерея
+
+    new Swiper('.testimonials__slider', {
+
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+
+            
+        },
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
+            }
+        }
+    });
+
 })()
